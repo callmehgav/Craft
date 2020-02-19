a41280b80d4f7400119fab04894e11d0033fb93a
Gavin Anderson <ganderson96@live.com>
2020-02-18 19:28:02 -0500

    Added Plants

--- src/world.c
+++ src/world.c
@@ -27,7 +27,7 @@ void create_world(int p, int q, world_func func, void *arg) {
                 func(x, y, z, w * flag, arg);
             }
             if (w == 1) {
-                if (SHOW_PLANTS) /* {
+                if (SHOW_PLANTS)  {
                     // grass
                     if (simplex2(-x * 0.1, z * 0.1, 4, 0.8, 2) > 0.6) {
                         func(x, h, z, 17 * flag, arg);
@@ -36,7 +36,7 @@ void create_world(int p, int q, world_func func, void *arg) {
                     if (simplex2(x * 0.05, -z * 0.05, 4, 0.8, 2) > 0.7) {
                         int w = 18 + simplex2(x * 0.1, z * 0.1, 4, 0.8, 2) * 7;
                         func(x, h, z, w * flag, arg);
-                    }*/
+                    }
                 }
                 // trees
                 int ok = SHOW_TREES;
